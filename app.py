2685fafcf29e64bd848bd53fcb8703749c2391a3
Luigi Gabriel Antonio Quirino <lugabrielt325@gmail.com>
2026-09-24 14:56:14 -0300

    Estrutura Base Mesmo

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def pagina_inicial():
+
+    return render_template('index.html', titulo='Teste')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --- ULTIMA COISA DO ARQUIVO ---
+if __name__ == '__main__':
+    app.run(debug=True)
