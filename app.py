511a49243c37bec83d52b7da631941d2d521f68a
Luigi Gabriel Antonio Quirino <lugabrielt325@gmail.com>
2026-09-24 17:35:39 -0300

    CSS Aplicado, botões começando a funcionar

--- app.py
+++ app.py
@@ -5,25 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def pagina_inicial():
+    return render_template('index.html')
 
-    return render_template('index.html', titulo='Teste')
+@app.route('/produtos')
+def produtos():
+    return render_template('usuarios/produtos.html')
 
+@app.route('/carrinho')
+def carrinho():
+    return render_template('usuarios/carrinho.html')
 
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/novidades')
+def novidades():
+    return render_template('usuarios/novidades.html')
 
 
 
 
-# --- ULTIMA COISA DO ARQUIVO ---
+
+
 if __name__ == '__main__':
+    # debug=True ativa o recarregamento automático ao salvar o arquivo
+    # NUNCA use debug=True em produção (servidor público)
     app.run(debug=True)
